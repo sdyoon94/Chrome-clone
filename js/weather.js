ab2788836c22861b00e2649a6bda276fcc1b810b
윤성도 <sdyoon94@gmail.com>
2021-11-01 05:19:32 +0900

    날씨 정보 가져오기

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "7a150e7d8a578c38a7b9ce38eefffeb6";
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather");
+      const city = document.querySelector("#city");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} / ${data.main.feels_like}`;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
